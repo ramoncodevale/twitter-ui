00fe95475d92d6ef1db8f343e9ad445e7a458cf9
ramoncodevale <ramonvalentim88@gmail.com>
2024-07-11 12:40:26 -0300

    feat:  add Tweet component with styles

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,11 +1,41 @@
-export function Tweet(props){
-    console.log(props)
-    return(
-    <div>
-        <strong>{props.user}</strong> 
-        <p>{props.content}</p>
-        <button>like</button>
-      
-    </div>
+import { ArrowsClockwise, ChatCircle, Heart } from 'phosphor-react'
+import {Link} from 'react-router-dom'
+import './Tweet.css'
+
+interface TweetProps {
+  content: string
+}
+
+export function Tweet(props: TweetProps) {
+    return (
+        <Link to='/status' className="tweet">
+            <img src="https://github.com/ramoncodevale.png" alt="Ramon Valentim" />
+
+            <div className="tweet-content">
+                <div className="tweet-content-header">
+                    <strong>Ramon Valentim</strong>
+                    <span>@ramon88</span>
+                </div>
+
+                <p>{props.content}</p>
+
+                <div className="tweet-content-footer">
+                  <button type='button'>
+                    <ChatCircle />
+                    20
+                  </button>
+
+                  <button type='button'>
+                    <ArrowsClockwise />
+                    20
+                  </button>
+
+                  <button type='button'>
+                    <Heart />
+                    20
+                  </button>
+                </div>
+            </div>
+        </Link>
     )
 }
